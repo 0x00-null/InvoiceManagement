ee44232c0a3f198a6e51b7a26ae93136430f5638
Mohamed Kamel Bouzekria <medkamelbouzekria@gmail.com>
2020-06-12 08:13:50 +0100

    Add project files.

---
+++ InvoiceManagement.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using InvoiceManagement.Infrastructure.Data;
+using InvoiceManagement.Infrastructure.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InvoiceManagement.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.AddIdentityServer()
+                .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
+
+            services.AddAuthentication()
+                .AddIdentityServerJwt();
+            return services;
+        }
+    }
+}
---
+++ InvoiceManagement.WebApi/Program.cs
@@ -0,0 +1,36 @@
+using System;
+using InvoiceManagement.Infrastructure.Data;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace InvoiceManagement.WebApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+           var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope()) 
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<ApplicationDbContext>();
+                    context.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex,"An error occured while migrating or seeding the database");
+                }
+            };
+            host.Run();
+        }
+
+        public static IWebHostBuilder CreateHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
+    }
+}
